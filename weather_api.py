8b1fa341c79a2dd10d96d79ecb17ef458cf5a22c
Xenodium123 <160533962+Xenodium123@users.noreply.github.com>
2024-06-06 20:06:57 +0200

    weather_api.py

---
+++ weather_api.py
@@ -0,0 +1,18 @@
+import requests
+
+class WeatherAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "http://api.openweathermap.org/data/2.5/weather"
+
+    def get_weather(self, city):
+        params = {
+            'q': city,
+            'appid': self.api_key,
+            'units': 'metric'
+        }
+        response = requests.get(self.base_url, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
